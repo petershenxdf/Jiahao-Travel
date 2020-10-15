26d8945e7aeb94183cce871cda3903d11031cd03
petershenxdf <petershenxdf@gmail.com>
2020-10-14 19:35:06 -0500

    add new component travel-info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TravelInfoComponent } from './components/travel-info/travel-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelInfoComponent
   ],
   imports: [
     BrowserModule,
