574b47ac6799ecaff0056c0772bcfbf8acf65fe5
petershenxdf <petershenxdf@gmail.com>
2020-11-02 16:18:26 -0600

    finish homework 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TravelInfoComponent} from './components/travel-info/travel-info.component';
+import {AboutComponent} from './components/about/about.component'
+const routes: Routes = [
+  {path:'travel-info',component:TravelInfoComponent},
+  {path:'about',component:AboutComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[TravelInfoComponent,AboutComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TravelInfoComponent } from './components/travel-info/travel-info.component';
+
 import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelInfoComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
